d0597aa51078a8e39ea3ab16134630eb5d2181bc
Calum Bradbury <eeycb2@nottingham.ac.uk>
2020-06-04 11:38:52 +0100

    split into components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './homepageStyles.scss'
+import Directory from '../../components/directory/directoryComponent';
+const HomePage = () => {
+  return (
+    <div className="Homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
