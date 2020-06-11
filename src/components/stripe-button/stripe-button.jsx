cf09754680b7185daa5441fa96e648bdbc5675c2
Calum Bradbury <eeycb2@nottingham.ac.uk>
2020-06-11 20:06:50 +0100

    removing redux-logger from production build

--- src/components/stripe-button/stripe-button.jsx
+++ src/components/stripe-button/stripe-button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
+//stripe wants price in US cents
 const StripeCheckoutButton = ({ price }) => {
-   const priceForStripe = price * 100; //stripe wants price in US cents
+   const priceForStripe = price * 100; 
    const publishableKey = 'pk_test_51GsmrGBp28TBmKjuq5pjX1BYmJIzGiB50p4EcXtlNjs7eAIsUgTszN0gQCMXUZp83mKh6ZwaIJ1dkWayVf4XVozo00bZP7umTr';
     
    const onToken = token => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger'//Redux Middleware
 import rootReducer from './root-reducer';
 import { persistStore } from 'redux-persist';
 
-const middleWares = [logger];
+const middleWares = [];
+//If process.environment.node evironment is development, log redux actions
+if(process.env.NODE_ENV === 'development'){
+    middleWares.push(logger);//push logger into middleWares array
+}
 export const store = createStore(rootReducer, applyMiddleware(...middleWares))//Spread in all values in array as individual arguments
 
 export const persistor = persistStore(store);
