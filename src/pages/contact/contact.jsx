747213227bf76ecc923f716bfc14f676bb09f787
Calum Bradbury <eeycb2@nottingham.ac.uk>
2020-06-12 20:36:06 +0100

    contact page

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import './contact.styles.scss';
+import React from "react";
+import "./contact.styles.scss";
 
 const ContactPage = () => {
-    return(
-        <div>CONTACT</div>
-    );
-}
+  return (
+    <div className="contact-page">
+      <h1 className="title">CONTACT</h1>
+      <span className="explanation">
+        Please send all contact enquires to cs.bradbury@outlook.com.
+      </span>
+    </div>
+     
+  );
+};
 
 export default ContactPage;
